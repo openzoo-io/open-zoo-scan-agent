7eaa0cedbb6e76049161149583ec1a71d7adaac9
moohooooo <suwichalala@gmail.com>
2021-12-28 17:20:16 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -116,7 +116,7 @@ const eventMap = {
       if (trackedSC.includes(_event.address.toLowerCase())) {
         let data = { address: _event.address, to: '0x' + _event.topics[2].slice(-40), tokenID: parseInt(_event.topics[3])};
         await sleep(3000);
-        console.log('Slept done @'+ new Date())
+        console.log('Slept done @'+ new Date().toISOString())
         await callApi('handle721Transfer', data);
         
       }
